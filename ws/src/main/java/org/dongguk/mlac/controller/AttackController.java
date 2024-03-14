511b4d8abaf3352d75b6b97105624e72734ab818
kurtyoon <akakertcobain@naver.com>
2024-03-14 18:01:45 +0900

    feat: apply web server architecture

--- ws/src/main/java/org/dongguk/mlac/controller/AttackController.java
+++ ws/src/main/java/org/dongguk/mlac/controller/AttackController.java
@@ -2,6 +2,7 @@ package org.dongguk.mlac.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.dongguk.mlac.dto.common.ResponseDto;
+import org.dongguk.mlac.dto.request.FilterRequestDto;
 import org.dongguk.mlac.service.AttackService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ public class AttackController {
     private final AttackService attackService;
 
     @PostMapping("/attack-log")
-    public ResponseDto<?> saveAttackLog(@RequestBody String attackLog){
-        return ResponseDto.ok(null);
+    public ResponseDto<?> saveAttackLog(@RequestBody FilterRequestDto filterRequestDto){
+        return ResponseDto.ok(attackService.apply(filterRequestDto));
     }
 }
