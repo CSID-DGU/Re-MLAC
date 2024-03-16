ee4194b772242d5c79736cd20b5882bede24d329
nykoh2001 <nykoh2001@gmail.com>
2024-03-16 18:44:23 +0900

    Chore: remove useless import

--- filter/src/main/java/org/dongguk/mlac/domain/PacketResult.java
+++ filter/src/main/java/org/dongguk/mlac/domain/PacketResult.java
@@ -1,6 +1,5 @@
 package org.dongguk.mlac.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
