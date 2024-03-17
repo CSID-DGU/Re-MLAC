df560e759a3cd2bfb58d71f0d4d5e44c863ccc24
nykoh2001 <nykoh2001@gmail.com>
2024-03-17 19:32:53 +0900

    Fix: log controller 이름 수정

--- filter/src/main/java/org/dongguk/mlac/controller/FilterLogController.java
+++ filter/src/main/java/org/dongguk/mlac/controller/FilterLogController.java
@@ -4,19 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.dongguk.mlac.dto.common.ResponseDto;
 import org.dongguk.mlac.dto.request.FilterRequestDto;
 import org.dongguk.mlac.service.FirewallLogService;
+import org.dongguk.mlac.service.WebServerLogService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class FirewallLogController {
+public class FilterLogController {
 
     private final FirewallLogService firewallLogService;
+    private final WebServerLogService webServerLogService;
 
     @RequestMapping("/api/v1/filter")
     public ResponseDto<?> filter(@RequestBody FilterRequestDto filterRequestDto) {
         firewallLogService.filter(filterRequestDto);
+        webServerLogService.filter(filterRequestDto);
         return ResponseDto.ok(null);
     }
 }
