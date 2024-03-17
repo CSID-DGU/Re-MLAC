1c50dbb472d28c85a311320f677abd101d69ddec
kurtyoon <akakertcobain@naver.com>
2024-03-18 04:47:05 +0900

    remove: delete useless file

--- ws/src/main/java/org/dongguk/mlac/repository/AttackRepository.java
+++
@@ -1,7 +0,0 @@
-package org.dongguk.mlac.repository;
-
-import org.dongguk.mlac.domain.Attack;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttackRepository extends JpaRepository<Attack, Long> {
-}
